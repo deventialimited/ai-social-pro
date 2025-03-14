8b301eb0a1d085a583b6aa8fd9278d87c57f5aa8
Munawar Jamil <92437716+MunawarJamil@users.noreply.github.com>
2025-03-14 16:42:22 +0500

    rmoved dep error

--- frontend/src/components/profile.tsx
+++ frontend/src/components/profile.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
 import axios from "axios";
-// import
+// import remove
 const API_BASE_URL = import.meta.env.VITE_BASE_URL; //|| "http://localhost:5000"
 
 const DEFAULT_BUSINESS_DATA = {
