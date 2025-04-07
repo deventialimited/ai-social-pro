05a24bcc5025964d802d71d249076ca92de81698
@codewithkazmi <115881946+Moeen-Kazmi369@users.noreply.github.com>
2025-04-07 11:31:08 +0500

    Update main.jsx

--- project/src/main.jsx
+++ project/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { GoogleOAuthProvider } from "@react-oauth/google";
-
+// some
 createRoot(document.getElementById('root')).render(
   <>
   <GoogleOAuthProvider clientId="334445684697-p0hj5muaed53rquvet0svco0q5loo7c8.apps.googleusercontent.com">
