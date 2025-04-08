42d0faa8847d97f57f55d69a2841399a54d4c4cf
Mehtab Ahmed <95327561+Mehtab66@users.noreply.github.com>
2025-04-08 16:54:03 +0500

    m

--- project/src/components/PostCard.jsx
+++ project/src/components/PostCard.jsx
@@ -14,6 +14,7 @@ import {
 } from 'lucide-react';
 import { format } from 'date-fns';
 import { PostEditModal } from './PostEditModal';
+import { usegetAllPostsByDomainId } from '../libs/postService';
 
 export const PostCard = ({
   post,
