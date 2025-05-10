b70593cd211b6a713ea5350e91b87a764ac64739
Mehtab Ahmed <95327561+Mehtab66@users.noreply.github.com>
2025-05-10 15:02:30 +0500

    Pricing UI changed

--- project/src/PopUps/PricingModal.tsx
+++ project/src/PopUps/PricingModal.tsx
@@ -134,7 +134,7 @@ export default function PricingModal({ onClose, isOpen }: PricingModalProps) {
                     <div className="mt-8">
                       <button
                         type="button"
-                        className="w-full rounded-md bg-gradient-to-r from-blue-600 to-indigo-600 py-2 px-3 text-center text-sm font-semibold text-white shadow-sm hover:from-blue-500 hover:to-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 transition-all duration-200"
+                        className="w-full rounded-md bg-gradient-to-r from-blue-600 to-purple-600 py-2 px-3 text-center text-sm font-semibold text-white shadow-sm hover:from-blue-500 hover:to-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 transition-all duration-200"
                       >
                         Get Started
                       </button>
@@ -161,7 +161,7 @@ export default function PricingModal({ onClose, isOpen }: PricingModalProps) {
                     <div className="mt-8">
                       <button
                         type="button"
-                        className="w-full rounded-md bg-gradient-to-r from-blue-600 to-indigo-600 py-2 px-3 text-center text-sm font-semibold text-white shadow-sm hover:from-blue-500 hover:to-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 transition-all duration-200"
+                        className="w-full rounded-md bg-gradient-to-r from-blue-600 to-purple-600 py-2 px-3 text-center text-sm font-semibold text-white shadow-sm hover:from-blue-500 hover:to-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 transition-all duration-200"
                       >
                         Get Started
                       </button>
