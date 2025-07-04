fb2ef080d0267f7de22c9fe01f78c35e2ac3f013
Mehtab Ahmed <mehtabahmed7777777@gmail.com>
2025-07-03 22:00:54 -0700

    fixed

--- project/src/libs/domainService.js
+++ project/src/libs/domainService.js
@@ -1,5 +1,5 @@
-// const baseURL = "https://api.oneyearsocial.com";
-const baseURL = "http://localhost:5000";
+const baseURL = "https://api.oneyearsocial.com";
+// const baseURL = "http://localhost:5000";
 const API_URL = `${baseURL}/api/v1/domains`;
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
